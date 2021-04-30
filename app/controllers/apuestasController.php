d2dfcd5330bd58c2060829fa20b562b6b16932af
AlexGB <jhosepgarbe0331@gmail.com>
2021-04-30 04:04:51 -0500

    creacion de vistas

--- app/controllers/apuestasController.php
+++ app/controllers/apuestasController.php
@@ -1,60 +1,60 @@
 <?php
-class ApuestasController extends Controller{
+require_once 'interfaces/operaciones.php';
+class ApuestasController extends Controller implements Operaciones{
 	public function index(){
 		$this->renderView('apuestas/index');
 	}
 
-	public function EsperanzaMatematica($param=null){
-		if ($param==null) {
-			$val1=(float)isset($_POST['val1'])?$_POST['val1']:'';
-			$val2=(float)isset($_POST['val2'])? $_POST['val2']:'';
-			$v2 =(float) $val2 / 100;
-			$rpta=(float)$val1*(float)$v2;
+	public function EsperanzaMatematica($param = null)
+	{
+		if ($param == null) {
+			$val1 = (float)isset($_POST['val1']) ? $_POST['val1'] : '';
+			$val2 = (float)isset($_POST['val2']) ? $_POST['val2'] : '';
+			$v2 = (float) $val2 / 100;
+			$rpta = (float)$val1 * (float)$v2;
+		} else {
+			$val1 = isset($param[0]) ? $param[0] : '';
+			$val2 = isset($param[1]) ? $param[1] : '';
+			$v2 = (float) $val2 / 100;
+			$rpta = (float) $val1 * (float) $v2;
+		}
+		if ($rpta < 1) {
+			$mbox = "el juego es desfavorables para el apostador ";
+		} else {
+			$mbox = "el juego es favorable para el apostador";
+		}
+		$data = [
+			'val1' => $val1,
+			'val2' => $v2,
+			'v2' => $val2,
+			'rpta' => $rpta,
+			'mbox' => $mbox
 
-		}else{
-			$val1=isset($param[0])? $param[0]:'';
-			$val2=isset($param[1])?$param[1]:'';
-			$v2=(float) $val2/100;
-			$rpta =(float) $val1 *(float) $v2;
-		}
-		if ($rpta<1) {
-			$mbox="el juego es desfavorables para el apostador ";
-		}else{
-			$mbox="el juego es favorable para el apostador";
-		}
-		$data=[
-			'val1'=>$val1,
-			'val2'=>$v2,
-			'v2'=>$val2,
-			'rpta'=>$rpta,
-			'mbox'=>$mbox
-			
 
 		];
-		$this->renderView('apuestas/esperanzaMatematica',$data);
-
+		$this->renderView('apuestas/esperanzaMatematica', $data);
 	}
 
-
-	public function casaApuesta($param=null){
+	public function casaApuesta($param = null)
+	{
 		if ($param == null) {
 			$val1 = (float)isset($_POST['val1']) ? $_POST['val1'] : '';
 			// $val2 = (float)isset($_POST['val2']) ? $_POST['val2'] : '';
 			// $v2 = (float) $val2 / 100;
-			$rpta =1/ (float)$val1 *100;
+			$rpta = 1 / (float)$val1 * 100;
 
-			$r=round($rpta,0);
+			$r = round($rpta, 0);
 		} else {
 			$val1 = isset($param[0]) ? $param[0] : '';
 			// $val2 = isset($param[1]) ? $param[1] : '';
 			// $v2 = (float) $val2 / 100;
-			$rpta =1/ (float)$val1 * 100;
-			$r=round($rpta,0);
+			$rpta = 1 / (float)$val1 * 100;
+			$r = round($rpta, 0);
 		}
 		$mbox = "hay un {$r}% de probavilidad de la casa de apuesta ";
 		$data = [
 			'val1' => $val1,
-			
+
 			'rpta' => $r,
 			'mbox' => $mbox
 
@@ -62,41 +62,41 @@ class ApuestasController extends Controller{
 		];
 		$this->renderView('apuestas/casaApuesta', $data);
 	}
-
-	public function gananciaNeta($param=null){
-		if ($param==null) {
+	public function gananciaNeta($param = null)
+	{
+		if ($param == null) {
 			$val1 = (float)isset($_POST['val1']) ? $_POST['val1'] : '';
 			$val2 = (float)isset($_POST['val2']) ? $_POST['val2'] : '';
-			
-			$rpta = (float)$val1 * ((float)$val2-1);
-		}else{
+
+			$rpta = (float)$val1 * ((float)$val2 - 1);
+		} else {
 			$val1 = isset($param[0]) ? $param[0] : '';
 			$val2 = isset($param[1]) ? $param[1] : '';
-			$rpta= (float)$val1 * (float)($val2 - 1);
+			$rpta = (float)$val1 * (float)($val2 - 1);
 		}
-		$mbox="la ganancia neta es de {$rpta} soles";
-		$data=[
-			'val1'=>$val1,
-			'val2'=>$val2,
-			'rpta'=>$rpta,
-			'mbox'=>$mbox
+		$mbox = "la ganancia neta es de {$rpta} soles";
+		$data = [
+			'val1' => $val1,
+			'val2' => $val2,
+			'rpta' => $rpta,
+			'mbox' => $mbox
 		];
-		$this->renderView('apuestas/gananciaNeta',$data);
-
+		$this->renderView('apuestas/gananciaNeta', $data);
 	}
-	public function retornoDeInvercion($param=null){
+	public function retornoDeInvercion($param = null)
+	{
 		if ($param == null) {
 			$gnta = (float)isset($_POST['val1']) ? $_POST['val1'] : '';
 			$intotal = (float)isset($_POST['val2']) ? $_POST['val2'] : '';
 
-			$rpta=(float) $gnta  /(float) $intotal *100;
+			$rpta = (float) $gnta  / (float) $intotal * 100;
 		} else {
 			$gnta = isset($param[0]) ? $param[0] : '';
 			$intotal = isset($param[1]) ? $param[1] : '';
 
-			$rpta = (float) $intotal /(float) $intotal * 100;
+			$rpta = (float) $intotal / (float) $intotal * 100;
 		}
-		$res=round($rpta,0);
+		$res = round($rpta, 0);
 		$mbox = "el retorno de inversion es de  {$res} centimos";
 		$data = [
 			'val1' => $gnta,
@@ -106,5 +106,6 @@ class ApuestasController extends Controller{
 		];
 		$this->renderView('apuestas/retornoDeInvercion', $data);
 	}
+	
 	
 }
---
+++ app/controllers/interfaces/operaciones.php
@@ -0,0 +1,9 @@
+<?php
+interface Operaciones{
+public function EsperanzaMatematica($param);
+public function casaApuesta($param);
+public function gananciaNeta($param);
+public function retornoDeInvercion($param);
+
+	
+}
